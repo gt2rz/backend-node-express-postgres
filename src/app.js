299a5d505247a5b24dc378e3dba863a8f45d961b
Miguel Gutierrez <gt2rz.dev@gmail.com>
2023-01-11 18:19:01 -0500

    add cors all routes

--- src/app.js
+++ src/app.js
@@ -1,16 +1,19 @@
-import express from 'express';
-import * as dotenv from 'dotenv'
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
 
 const app = express();
 
-dotenv.config()
+app.use(cors());
+
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/',(req, res)=>{
-    res.send('Hello World');
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
-app.listen(PORT, ()=>{
-    console.log(`Server started on port ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
